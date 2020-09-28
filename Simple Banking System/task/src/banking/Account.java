ecee49cdd929936a3f58dfc3740497b27ab801d8
annascott <workwithpurpose@yahoo.com>
2020-09-27 21:07:35 -0600

    2/4

--- Simple Banking System/task/src/banking/Account.java
+++ Simple Banking System/task/src/banking/Account.java
@@ -11,7 +11,6 @@ public class Account {
         String cardNumber = "400000";
         String pinNumber;
         File file = new File("account.txt");
-        //FileWriter writer = new FileWriter(file);
 
         private Card() throws IOException {
         }
@@ -34,20 +33,52 @@ public class Account {
             return String.valueOf(stringBulder);
         }
 
-        String lastDigit = "0";
+        public String generateAccountLuhn(){
+            int sum = 0;
+            String account = generateAccount();
+            String[] accountString = account.split("");
+            int[] accountInt = new int[accountString.length + 6];
+            accountInt[0] = 4;
+            accountInt[1] = 0;
+            accountInt[2] = 0;
+            accountInt[3] = 0;
+            accountInt[4] = 0;
+            accountInt[5] = 0;
+            for (int i = 0; i < accountString.length; i++) {
+                accountInt[i + 6] = Integer.parseInt(accountString[i]);
+            }
+            //System.out.println(Arrays.toString(accountInt));
+            for (int i = 0; i < accountInt.length; i+=2) {
+                accountInt[i] = accountInt[i] * 2;
+
+                if (accountInt[i] > 9) {
+                    accountInt[i] = accountInt[i] - 9;
+                }
+
+                //sum = sum + accountInt[i];
+            }
+
+            for (int each : accountInt) {
+                sum = sum + each;
+            }
+
+            int lastDigit = 0;
+            if (sum % 10 != 0) {
+                do {
+                    lastDigit = lastDigit + 1;
+                } while ((sum + lastDigit) % 10 != 0);
+            }
+            return account + lastDigit;
+        }
 
         public void generateCard() throws IOException {
-            cardNumber = cardNumber + generateAccount() + lastDigit;
+            cardNumber = cardNumber + generateAccountLuhn();
             pinNumber = generatePin();
             System.out.printf("Your card has been created \nYour card number: \n%s \nYour card PIN: \n%s\n", cardNumber, pinNumber);
-
             System.out.println();
+                double balance = 0;
+                writeIntoFile(cardNumber, pinNumber, balance);
 
-            //check if file empty and write in an account information, otherwise append
-            BufferedReader br = new BufferedReader(new FileReader("account.txt"));
-
-            double balance = 0;
-            writeIntoFile(cardNumber, pinNumber, balance);
         }
 
         public void writeIntoFile(String cardNumber, String pinNumber, double balance) throws IOException {
